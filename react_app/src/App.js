5f5f01da4344c76add7c8bd9fdadd33171380f60
PedroAmaroImproove <69147803+PedroAmaroImproove@users.noreply.github.com>
2021-02-27 02:18:37 +0000

    [LeftMenu]

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
-import './App.css';
+import me from './images/PEDROAMARO.png'
+import arrow from './images/arrow.svg'
+import $ from 'jquery';
+import './App.scss';
 
 function App() {
+  function handleMenu(e) {
+    console.log('The link was clicked.');
+    $("#left-container").css("transform", 'translate(0px,0px)');
+    $("#menu-button-img").style.transform = "180deg";
+    $("#menu-button-img").style.transformOrigin = "center";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="body App">
+      <div className="App-header left-container" id="left-container">
+        <a className="arrow-container" onClick={handleMenu} href="#" id="menu-button">
+          <img src={arrow} className="arrow" id="menu-button-img"></img>
         </a>
-      </header>
+        <img src={me} className="App-logo" alt="logo" />
+        <div className="links-container">
+          <a className="links-container_about" 
+            href="">
+            About Me
+          </a>
+          <a className="links-container_resume" 
+            href="">
+            Resume
+          </a> 
+          <a className="links-container_contact" 
+            href="">
+            Contact
+          </a>
+        </div>
+      </div>
+      <div className="main-content">
+          content
+      </div>
     </div>
   );
 }
