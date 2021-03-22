0128bbb284e90463ca8989b71f1ab4f7e42ef7e0
PedroAmaroImproove <69147803+PedroAmaroImproove@users.noreply.github.com>
2021-03-22 23:05:07 +0000

    [Task-1] Menu component v.1

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,39 +1,12 @@
 import logo from './logo.svg';
-import me from './images/PEDROAMARO.png'
-import arrow from './images/arrow.svg'
-import $ from 'jquery';
-import './App.scss';
+import './App.scss'
+
+import Menu from './components/Menu'
 
 function App() {
-  function handleMenu(e) {
-    console.log('The link was clicked.');
-    $("#left-container").css("transform", 'translate(0px,0px)');
-    $("#menu-button-img").style.transform = "180deg";
-    $("#menu-button-img").style.transformOrigin = "center";
-  }
-
   return (
     <div className="body App">
-      <div className="App-header left-container" id="left-container">
-        <a className="arrow-container" onClick={handleMenu} href="#" id="menu-button">
-          <img src={arrow} className="arrow" id="menu-button-img"></img>
-        </a>
-        <img src={me} className="App-logo" alt="logo" />
-        <div className="links-container">
-          <a className="links-container_about" 
-            href="">
-            About Me
-          </a>
-          <a className="links-container_resume" 
-            href="">
-            Resume
-          </a> 
-          <a className="links-container_contact" 
-            href="">
-            Contact
-          </a>
-        </div>
-      </div>
+      <Menu/>
       <div className="main-content">
           content
       </div>
---
+++ react_app/src/components/Menu.js
@@ -0,0 +1,46 @@
+import $ from 'jquery';
+import me from '../images/PEDROAMARO.png'
+import arrow from '../images/arrow.svg'
+
+const Menu = () => {
+
+    let state = true;
+
+    function handleMenu() {
+        console.log('The link was clicked.');
+        if (state) {
+            $("#left-container").addClass('closed');
+            $("#menu-button-img").addClass('closed');
+            state=false;
+        } else {
+            $("#left-container").removeClass('closed');
+            $("#menu-button-img").removeClass('closed');
+            state=true;
+        } 
+    }
+
+    return (
+        <div className="App-header left-container" id="left-container">
+            <a className="arrow-container" onClick={handleMenu} href="#" id="menu-button">
+            <img src={arrow} className="arrow" id="menu-button-img"></img>
+            </a>
+            <img src={me} className="my-photo" alt="logo" />
+            <div className="links-container">
+                <a className="links-container_about" 
+                    href="">
+                    About Me
+                </a>
+                <a className="links-container_resume" 
+                    href="">
+                    Resume
+                </a> 
+                <a className="links-container_contact" 
+                    href="">
+                    Contact
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Menu
