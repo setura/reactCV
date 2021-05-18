c7c0360b8db57d325fe14f7a0f3a4f07114f85c8
PedroAmaroImproove <69147803+PedroAmaroImproove@users.noreply.github.com>
2021-05-18 21:29:48 +0100

    [ReactCV-005] Added the resume and contact func, also wrote some more information about university projects

--- react_app/src/components/Resume.js
+++ react_app/src/components/Resume.js
@@ -1,11 +1,11 @@
 import $ from 'jquery';
-
+import cv from '../resume/CV-02-21-Zurich.pdf'
 
 const Resume = () => {
 
     return (
         <div className="resume-me-page">
-            <h1>resume ME</h1>
+            <embed src={cv} type="application/pdf" width="100%" height="100%" />
         </div>
     )
 }
--- react_app/src/components/index.js
+++ react_app/src/components/index.js
@@ -1,3 +1,2 @@
 export { default as About } from "./About";
 export { default as Resume } from "./Resume";
-export { default as Contact } from "./Contact";
